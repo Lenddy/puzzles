0499a7825f4b920110060d7ea2548b02d437e6c7
Lenddy <leme2003@gmail.com>
2022-08-02 20:03:00 -0400

    puzzles

--- Program.cs
+++ Program.cs
@@ -6,23 +6,9 @@ static int[]  RandomArray(){
         someArray[i] = rand.Next(5,25);
         Console.WriteLine(someArray[i]);
     }
-        int a,max,min,n;
-        n =10;
-        max = someArray[0];
-        min = someArray[0];
-        for(a = 1; a < n ; a++){
-            if(someArray[a] > max){
-                max = someArray[a];
-            }
-            if(someArray[a] < min ){
-                min = someArray[a];
-            }
-        }
-        
         int sum = someArray.Sum();
-        
-        Console.WriteLine($" this is the max value in the array {max} .");
-        Console.WriteLine($" this is the min value in the array {min} .");
+        Console.WriteLine($" this is the max value in the array {someArray.Max()} .");
+        Console.WriteLine($" this is the min value in the array {someArray.Min()} .");
         Console.WriteLine($"this is the sum of all the values in teh array {sum} .");
         return someArray;
 } 
