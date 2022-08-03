7b02d0d2021532ed98a9a524c86be84a572a30fb
Lenddy <leme2003@gmail.com>
2022-08-02 21:49:59 -0400

    puzzles

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿
-static int[]  RandomArray(){
+﻿static int[]  RandomArray(){
     int[] someArray = new int[10];
     Random rand = new Random();
     for(int i = 0;i < 10;i++){
@@ -7,10 +6,42 @@ static int[]  RandomArray(){
         Console.WriteLine(someArray[i]);
     }
         int sum = someArray.Sum();
-        Console.WriteLine($" this is the max value in the array {someArray.Max()} .");
-        Console.WriteLine($" this is the min value in the array {someArray.Min()} .");
-        Console.WriteLine($"this is the sum of all the values in teh array {sum} .");
+        Console.WriteLine($" this is the max value in the array {someArray.Max()}.");
+        Console.WriteLine($" this is the min value in the array {someArray.Min()}.");
+        Console.WriteLine($"this is the sum of all the values in teh array {sum}.");
         return someArray;
 } 
 
 RandomArray();
+
+
+static int TossCoin(){
+    Random rand = new Random();
+    Console.WriteLine("Tossing Coin");
+    int n = rand.Next(1,3);
+        if(n == 1){
+            Console.WriteLine("the coin landed on Heads");
+        }
+        else if(n == 2){
+            Console.WriteLine("the coin landed on Tails");
+        }
+        else{
+        Console.WriteLine("is a draw some how  toss again");
+        }
+        return n ;
+    }
+
+TossCoin();
+
+
+static List<string> Names(){
+    List<string> listNames = new List<string> {"Todd", "Tiffany", "Charlie", "Geneva", "Sydney"};
+    foreach(string item in listNames){
+        if(item.Length > 5){
+            Console.WriteLine(item);
+        }
+    }
+    return listNames;
+}
+
+Names();
